594fee41a53a41a077f07eb7c132da2f0dbe854b
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2021-03-28 17:54:10 +0800

    优化日志系统

--- src/LogUtil.java
+++ src/LogUtil.java
@@ -1,43 +1,54 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class LogUtil {
 	static byte[] all = new byte[0];
-	public static void Log(String log){
+	static File file;
+	public static void init() {
 		try {
-			SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd");
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 			Date date = new Date(System.currentTimeMillis());
 			String time = formatter.format(date);
 			File dir = new File("logs");
-			if (!dir.exists()){
-				if (!dir.mkdirs()){
+			if (!dir.exists()) {
+				if (!dir.mkdirs()) {
 					System.out.println(time + "创建配置文件失败！即将终止进程！");
 					System.exit(-1);
 					return;
 				}
 			}
-			File file = new File("logs" + File.separator + time + ".log");
-			if (!file.exists()){
-				if (!file.createNewFile()){
+			file = new File("logs" + File.separator + time + ".log");
+			if (!file.exists()) {
+				if (!file.createNewFile()) {
 					System.out.println(time + "创建配置文件失败！即将终止进程！");
 					System.exit(-1);
 					return;
 				}
 			}
-			formatter= new SimpleDateFormat("[HH:mm:ss] ");
-			date = new Date(System.currentTimeMillis());
-			time = formatter.format(date);
 			FileInputStream fis = new FileInputStream(file);
 			all = fis.readAllBytes();
 			FileOutputStream fos = new FileOutputStream(file);
-			all = byteMerger(all, (time + log + "\n").getBytes());
 			fos.write(all);
 			fos.flush();
-			System.out.println(time + log);
+		} catch (IOException e) {
+			System.out.println("IO 流 抛出错误，即将终止进程！");
+			System.out.println("(" + e.getCause() + " : " + e.getMessage() + ")");
+			System.exit(-1);
+		}
+	}
+	public static void log(String str){
+		try {
+			SimpleDateFormat formatter = new SimpleDateFormat("[HH:mm:ss] ");
+			Date date = new Date(System.currentTimeMillis());
+			String time = formatter.format(date);
+			FileInputStream fis = new FileInputStream(file);
+			all = fis.readAllBytes();
+			FileOutputStream fos = new FileOutputStream(file);
+			all = byteMerger(all, (time + str + "\n").getBytes());
+			fos.write(all);
+			fos.flush();
+			System.out.println(time + str);
 		} catch (IOException e) {
 			System.out.println("IO 流 抛出错误，即将终止进程！");
 			System.out.println("(" + e.getCause() + " : " + e.getMessage() + ")");
@@ -46,25 +57,6 @@ public class LogUtil {
 	}
 	public static void Exit(){
 		try {
-			SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd");
-			Date date = new Date(System.currentTimeMillis());
-			String time = formatter.format(date);
-			File dir = new File("logs");
-			if (!dir.exists()){
-				if (!dir.mkdirs()){
-					System.out.println(time + "创建配置文件失败！即将终止进程！");
-					System.exit(-1);
-					return;
-				}
-			}
-			File file = new File("logs" + File.separator + time + ".log");
-			if (!file.exists()){
-				if (!file.createNewFile()){
-					System.out.println(time + "创建日志文件失败！即将终止进程！");
-					System.exit(-1);
-					return;
-				}
-			}
 			FileInputStream fis = new FileInputStream(file);
 			all = fis.readAllBytes();
 			FileOutputStream fos = new FileOutputStream(file);
