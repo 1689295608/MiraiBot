0dd19b9ce1cf530c5580f2aad8ef6c3d06dd886d
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2021-04-02 19:01:45 +0800

    Update IniUtil.java

--- src/IniUtil.java
+++ src/IniUtil.java
@@ -6,28 +6,26 @@ public class IniUtil {
 	private static final Map<String, HashMap<String, String>> sectionsMap = new HashMap<>();
 	private static HashMap<String, String> itemsMap = new HashMap<>();
 	private static String currentSection = "";
-	private static void loadData(File file) {
+	
+	public static void loadData(File file) {
 		BufferedReader reader = null;
 		try {
 			reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
 			String line;
 			while ((line = reader.readLine()) != null) {
 				line = line.trim();
-				if ("".equals(line))
+				if (line.isEmpty() || line.startsWith("#") || line.startsWith("//")) {
 					continue;
+				}
 				if (line.startsWith("[") && line.endsWith("]")) {
-					if (itemsMap.size() > 0 && !"".equals(currentSection.trim())) {
+					if (itemsMap.size() > 0 && !currentSection.trim().isEmpty()) {
 						sectionsMap.put(currentSection, itemsMap);
 					}
-					currentSection = "";
-					itemsMap = null;
 					currentSection = line.substring(1, line.length() - 1);
 					itemsMap = new HashMap<>();
-				}
-				else {
+				} else {
 					int index = line.indexOf("=");
-					if (index != -1)
-					{
+					if (index != -1){
 						String key = line.substring(0, index);
 						String value = line.substring(index + 1);
 						itemsMap.put(key, value);
@@ -35,24 +33,20 @@ public class IniUtil {
 				}
 			}
 			reader.close();
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			if (reader != null) {
 				try {
 					reader.close();
-				}
-				catch (IOException e) {
+				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
 		}
 	}
 	
-	public static String getValue(String section, String item, File file) {
-		loadData(file);
+	public static String getValue(String section, String item) {
 		HashMap<String, String> map = sectionsMap.get(section);
 		if (map == null) {
 			return null;
@@ -60,15 +54,14 @@ public class IniUtil {
 		return map.get(item);
 	}
 	
-	public static List<String> getSectionNames(File file) {
-		loadData(file);
-		Set<String> key = sectionsMap.keySet();
-		return new ArrayList<>(key);
+	public static ArrayList<String> getSectionNames() {
+		return new ArrayList<>(sectionsMap.keySet());
 	}
-	
-	public static Map<String, String> getItemsBySectionName(String section, File file) {
-		loadData(file);
-		return sectionsMap.get(section);
-	}
+	/*
+	 暂时不用
+	 public static Map<String, String> getItemsBySectionName(String section) {
+	 	return sectionsMap.get(section);
+	 }
+	*/
 }
 
