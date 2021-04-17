113432d9e17ad86b572b610e4613dd323217d3a2
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2021-04-17 18:17:39 +0800

    优化配置系统

--- src/ConfigUtil.java
+++ src/ConfigUtil.java
@@ -1,28 +1,30 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigUtil {
+	public static Properties config = new Properties();
+	public static Properties language = new Properties();
+	
+	/**
+	 * Initialize the config system
+	 */
+	public static void init() {
+		try {
+			config.load(new BufferedReader(new FileReader("config.properties")));
+			language.load(new BufferedReader(new FileReader("language.properties")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	/**
 	 * Get the value of key in the configuration file
 	 * @param key Key
 	 * @return Config value
 	 */
 	public static String getConfig(String key) {
-		Properties p = new Properties();
-		try {
-			BufferedReader bufferedReader = new BufferedReader(new FileReader("config.properties"));
-			try {
-				p.load(bufferedReader);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return p.getProperty(key);
+		return config.getProperty(key);
 	}
 	
 	/**
@@ -31,17 +33,6 @@ public class ConfigUtil {
 	 * @return Language value
 	 */
 	public static String getLanguage(String key) {
-		Properties p = new Properties();
-		try {
-			BufferedReader bufferedReader = new BufferedReader(new FileReader("language.properties"));
-			try {
-				p.load(bufferedReader);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return p.getProperty(key);
+		return language.getProperty(key);
 	}
 }
