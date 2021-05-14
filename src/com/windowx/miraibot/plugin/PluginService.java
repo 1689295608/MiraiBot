f34024dd986fc39efb117924903b9189a6514cf7
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2021-05-14 22:41:34 +0800

    优化，不用强制覆写所有方法

--- src/com/windowx/miraibot/plugin/PluginService.java
+++ src/com/windowx/miraibot/plugin/PluginService.java
@@ -1,14 +1,9 @@
 package com.windowx.miraibot.plugin;
 
-public interface PluginService {
-	void onEnable();
+public abstract class PluginService {
+	public boolean onCommand(String cmd) {
+		return true;
+	}
 	
-	/**
-	 * Triggered when the instruction is executed,
-	 * it is not recommended to return false anyway,
-	 * it will cause the user to be unable to send messages directly.
-	 * @param cmd Command
-	 * @return Whether to allow the message to be sent
-	 */
-	boolean onCommand(String cmd);
+	public void onEnable() { }
 }
