d5f0781574af393c1e0f164d485f07135a28d059
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2021-05-29 22:55:11 +0800

    增加便捷的 onGroupMessage() 函数

--- src/com/windowx/miraibot/plugin/PluginService.java
+++ src/com/windowx/miraibot/plugin/PluginService.java
@@ -1,9 +1,16 @@
 package com.windowx.miraibot.plugin;
 
-public abstract class PluginService {
+import com.windowx.miraibot.utils.LogUtil;
+import net.mamoe.mirai.event.events.GroupMessageEvent;
+
+public abstract class PluginService extends Plugin {
 	public boolean onCommand(String cmd) {
 		return true;
 	}
 	
-	public void onEnable() { }
+	public void onEnable() {
+		LogUtil.log("[" + this.getName() + "] Enabling " + this.getName() + "...");
+	}
+	
+	public void onGroupMessage(GroupMessageEvent event) { }
 }
