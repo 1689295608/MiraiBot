fd245ea2459cbcd0c26f3c3ab3b0b66406bb10df
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2021-06-21 12:02:37 +0800

    没有注释的代码是没有灵魂的

--- src/com/windowx/miraibot/plugin/PluginCore.java
+++ src/com/windowx/miraibot/plugin/PluginCore.java
@@ -6,6 +6,13 @@ import java.util.Properties;
 public class PluginCore {
 	static Properties properties = new Properties();
 	public static Plugin plugin;
+	
+	/**
+	 * Modify the value of key in the configuration file
+	 * @param key Key
+	 * @param value Value
+	 * @throws IOException IOException
+	 */
 	public void setConfig(String key, String value) throws IOException {
 		File config = new File("plugins/" + plugin.getName() + "/");
 		if (!config.exists()) {
@@ -23,6 +30,13 @@ public class PluginCore {
 		properties.setProperty(key, value);
 	}
 	
+	/**
+	 * Get the value of key in the configuration file
+	 * @param key Key
+	 * @param defaultValue Default Value
+	 * @return Value
+	 * @throws IOException IOException
+	 */
 	public String getConfig(String key, String defaultValue) throws IOException {
 		File config = new File("plugins/" + plugin.getName() + "/config.ini");
 		if (!config.exists()) {
@@ -32,6 +46,10 @@ public class PluginCore {
 		return properties.getProperty(key, defaultValue);
 	}
 	
+	/**
+	 * Save configuration file to file
+	 * @throws IOException IOException
+	 */
 	public void saveConfig() throws IOException {
 		File config = new File("plugins/" + plugin.getName() + "/");
 		if (!config.exists()) {
@@ -50,18 +68,36 @@ public class PluginCore {
 		fos.close();
 	}
 	
+	/**
+	 * Get whether the key is included in the configuration file
+	 * @param key Key
+	 * @return Whether to include the key
+	 */
 	public boolean hasKey(String key) {
 		return properties.containsKey(key);
 	}
 	
+	/**
+	 * Get whether the value is included in the configuration file
+	 * @param value Value
+	 * @return Whether to include the value
+	 */
 	public boolean hasValue(String value) {
 		return properties.containsValue(value);
 	}
 	
+	/**
+	 * Get current properties
+	 * @return Current properties
+	 */
 	public Properties getProperties() {
 		return properties;
 	}
 	
+	/**
+	 * Set current properties
+	 * @param properties Properties
+	 */
 	public void setProperties(Properties properties) {
 		PluginCore.properties = properties;
 	}
