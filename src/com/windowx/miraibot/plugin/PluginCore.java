7f20cb05e0438479c0b61493f8a01d369a1a6b56
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2021-06-22 18:38:38 +0800

    使用玄学

--- src/com/windowx/miraibot/plugin/PluginCore.java
+++ src/com/windowx/miraibot/plugin/PluginCore.java
@@ -13,7 +13,7 @@ public class PluginCore {
 	 * @param value Value
 	 * @throws IOException IOException
 	 */
-	public void setConfig(String key, String value) throws IOException {
+	@Deprecated public void setConfig(String key, String value) throws IOException {
 		File config = new File("plugins/" + plugin.getName() + "/");
 		if (!config.exists()) {
 			if (!config.mkdirs()) {
@@ -30,6 +30,23 @@ public class PluginCore {
 		properties.setProperty(key, value);
 	}
 	
+	public void setConfigByName(String name, String key, String value) throws IOException {
+		File config = new File("plugins/" + name + "/");
+		if (!config.exists()) {
+			if (!config.mkdirs()) {
+				throw new IOException("Cloud not create dirs: " + config);
+			}
+		}
+		config = new File("plugins/" + name + "/config.ini");
+		if (!config.exists()) {
+			if (!config.createNewFile()) {
+				throw new IOException("Cloud not create config file: " + config);
+			}
+		}
+		properties.load(new FileReader(config));
+		properties.setProperty(key, value);
+	}
+	
 	/**
 	 * Get the value of key in the configuration file
 	 * @param key Key
@@ -37,8 +54,17 @@ public class PluginCore {
 	 * @return Value
 	 * @throws IOException IOException
 	 */
-	public String getConfig(String key, String defaultValue) throws IOException {
+	@Deprecated public String getConfig(String key, String defaultValue) throws IOException {
 		File config = new File("plugins/" + plugin.getName() + "/config.ini");
+		if (!config.exists()) {
+			return defaultValue;
+		}
+		properties.load(new FileReader(config));
+		return properties.getProperty(key, defaultValue);
+	}
+	
+	public String getConfigByName(String name, String key, String defaultValue) throws IOException {
+		File config = new File("plugins/" + name + "/config.ini");
 		if (!config.exists()) {
 			return defaultValue;
 		}
@@ -50,7 +76,7 @@ public class PluginCore {
 	 * Save configuration file to file
 	 * @throws IOException IOException
 	 */
-	public void saveConfig() throws IOException {
+	@Deprecated public void saveConfig() throws IOException {
 		File config = new File("plugins/" + plugin.getName() + "/");
 		if (!config.exists()) {
 			if (!config.mkdirs()) {
@@ -58,6 +84,24 @@ public class PluginCore {
 			}
 		}
 		config = new File("plugins/" + plugin.getName() + "/config.ini");
+		if (!config.exists()) {
+			if (!config.createNewFile()) {
+				throw new IOException("Cloud not create config file: " + config);
+			}
+		}
+		FileOutputStream fos = new FileOutputStream(config);
+		properties.store(fos, null);
+		fos.close();
+	}
+	
+	public void saveConfigByName(String name) throws IOException {
+		File config = new File("plugins/" + name + "/");
+		if (!config.exists()) {
+			if (!config.mkdirs()) {
+				throw new IOException("Cloud not create dirs: " + config);
+			}
+		}
+		config = new File("plugins/" + name + "/config.ini");
 		if (!config.exists()) {
 			if (!config.createNewFile()) {
 				throw new IOException("Cloud not create config file: " + config);
@@ -94,11 +138,27 @@ public class PluginCore {
 		return properties;
 	}
 	
+	public Properties getPropertiesByName(String name) throws IOException {
+		File config = new File("plugins/" + name + "/config.ini");
+		properties.load(new FileReader(config));
+		return properties;
+	}
+	
 	/**
 	 * Set current properties
 	 * @param properties Properties
 	 */
 	public void setProperties(Properties properties) {
 		PluginCore.properties = properties;
+	}
+	
+	@Deprecated public String getName() {
+		return plugin.getName();
+	}
+	@Deprecated public String getClassName() {
+		return plugin.getClassName();
+	}
+	@Deprecated public String getJarPath() {
+		return plugin.getJar();
 	}
 }
