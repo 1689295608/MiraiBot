dadc497452eb56c2b87e498b9e7eab83b1333efc
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2021-07-17 17:14:49 +0800

    优化插件系统

--- src/com/windowx/miraibot/plugin/Plugin.java
+++ src/com/windowx/miraibot/plugin/Plugin.java
@@ -1,25 +1,52 @@
 package com.windowx.miraibot.plugin;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Plugin extends JavaPlugin {
 	private String name;
+	private String owner;
 	private String className;
-	private Plugin instance;
+	private Properties config;
+	public void setConfig(Properties properties) {
+		this.config = properties;
+	}
+	public Properties getConfig() {
+		return this.config;
+	}
+	public void saveConfig() throws IOException {
+		File file = new File("plugins/" + name + "/");
+		if (!file.exists()) {
+			if (!file.mkdirs()) {
+				throw new IOException("Cloud not create dirs: " + config);
+			}
+		}
+		file = new File("plugins/" + name + "/config.ini");
+		if (!file.exists()) {
+			if (!file.createNewFile()) {
+				throw new IOException("Cloud not create config file: " + config);
+			}
+		}
+		config.store(new FileOutputStream(file), null);
+	}
 	public void setName(String name) {
 		this.name = name;
 	}
 	public String getName() {
 		return this.name;
 	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	public String getOwner() {
+		return this.owner;
+	}
 	public void setClassName(String className) {
 		this.className = name;
 	}
 	public String getClassName() {
 		return this.className;
-	}
-	public void setInstance(Plugin instance) {
-		this.instance = instance;
-	}
-	public Plugin getInstance() {
-		return this.instance;
 	}
 }
