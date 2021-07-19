b8c6cbf56b59d5da95554920b9ec4a9dd5f33547
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2021-07-19 17:49:13 +0800

    优化
    都说了多少遍，不要额外的定义插件名！
    还有你的恶魔级别缩进，我差点看目害了

--- docs/plugin/example.java
+++ docs/plugin/example.java
@@ -7,24 +7,28 @@ import net.mamoe.mirai.message.data.At;
 import net.mamoe.mirai.message.data.*;
 
 public class GrassReply extends Plugin {
-	String PluginName = "Grass Reply";
-
-	@Override public void onEnable(){
-	loginfo("Grass Reply插件已加载!");
+	@Override
+	public void onEnable(){
+		log(this.getName() + " 已加载!");
         }
 
-        @Override public void onDisable(){
-	loginfo("Grass Reply插件已卸载!");
+        @Override
+	public void onDisable(){
+		log(this.getName() + " 插件已卸载!");
 	}
 
-	@Override public void onGroupMessage(GroupMessageEvent event){
-	String msg = event.getMessage().contentToString();
-	if(msg.equals("草") || msg.equals("艹")){
-                 event.getGroup().sendMessage(new QuoteReply(event.getSource()) .plus("(一种植物)"));
-	}
+	@Override
+	public void onGroupMessage(GroupMessageEvent event){
+		String msg = event.getMessage().contentToString();
+		if(msg.equals("草") || msg.equals("艹")){
+        	         event.getGroup().sendMessage(
+				 new QuoteReply(event.getSource())
+				 .plus("(一种植物)")
+			 );
+		}
         }
 
-	public void loginfo(String logmsg){
-	LogUtil.log("["+PluginName+"] "+logmsg);
+	public void log(String log){
+		LogUtil.log("[" + this.getName() + "] " + log);
 	}
 }
