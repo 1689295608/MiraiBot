d0daee8130d5c3f96e3f0f6752c0198a3bf5f283
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2021-08-07 00:58:02 +0800

    优化

--- src/com/windowx/miraibot/plugin/Plugin.java
+++ src/com/windowx/miraibot/plugin/Plugin.java
@@ -5,7 +5,6 @@ import com.windowx.miraibot.utils.LogUtil;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URLClassLoader;
 import java.util.Properties;
 
 public class Plugin extends JavaPlugin {
@@ -16,15 +15,15 @@ public class Plugin extends JavaPlugin {
 	private String version;
 	private String description;
 	private Properties config;
-	private URLClassLoader classLoader;
+	private ClassLoader classLoader;
 	private boolean isEnabled;
 	private Properties plugin;
 	
-	public URLClassLoader getClassLoader() {
+	public ClassLoader getClassLoader() {
 		return classLoader;
 	}
 	
-	public void setClassLoader(URLClassLoader classLoader) {
+	public void setClassLoader(ClassLoader classLoader) {
 		StackTraceElement[] stack = Thread.currentThread().getStackTrace();
 		for (StackTraceElement ste : stack) {
 			if (!ste.getClassName().equals("java.lang.Thread") && !ste.getClassName().equals("com.windowx.miraibot.PluginMain") &&
@@ -123,15 +122,15 @@ public class Plugin extends JavaPlugin {
 		this.description = description;
 	}
 	
-	public void info(String info) {
-		LogUtil.log("[" + name + "] " + info);
+	public void info(String info, String... args) {
+		LogUtil.log("[" + name + "] " + info, args);
 	}
 	
-	public void error(String error) {
-		LogUtil.error("[" + name + "] " + error);
+	public void error(String error, String... args) {
+		LogUtil.error("[" + name + "] " + error, args);
 	}
 	
-	public void warn(String warn) {
-		LogUtil.warn("[" + name + "] " + warn);
+	public void warn(String warn, String... args) {
+		LogUtil.warn("[" + name + "] " + warn, args);
 	}
 }
