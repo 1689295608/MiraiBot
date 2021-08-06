66b3f7485058ccf4aca821ced9af937ccfe3a258
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2021-08-06 13:40:25 +0800

    精简化（

--- docs/plugin/example.java
+++ docs/plugin/example.java
@@ -1,12 +1,8 @@
 package miraibot;
-import java.util.*;
+
 import com.windowx.miraibot.plugin.Plugin;
-import net.mamoe.mirai.event.events.GroupMessageEvent;
-import net.mamoe.mirai.message.data.QuoteReply;
 
 public class example extends Plugin {
-        //编译后会存在于 /miraibot/example.class 主类为miraibot.example
-
 	@Override
 	public void onEnable(){
 		this.info(this.getName() + " 已加载!");
@@ -16,15 +12,4 @@ public class example extends Plugin {
 	public void onDisable(){
 		this.info(this.getName() + " 插件已卸载!");
 	}
-
-	@Override
-	public void onGroupMessage(GroupMessageEvent event){
-		String msg = event.getMessage().contentToString();
-		if(msg.equals("草") || msg.equals("艹")){
-        	         event.getGroup().sendMessage(
-				 new QuoteReply(event.getSource())
-				 .plus("(一种植物)")
-			 );
-		}
-        }
 }
