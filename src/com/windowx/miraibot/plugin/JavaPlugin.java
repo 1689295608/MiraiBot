a95bafa15fa8da02f27e6c991cfeb4c22986c4d1
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2021-08-06 20:51:28 +0800

    增加注释

--- src/com/windowx/miraibot/plugin/JavaPlugin.java
+++ src/com/windowx/miraibot/plugin/JavaPlugin.java
@@ -3,19 +3,33 @@ package com.windowx.miraibot.plugin;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
 
 public abstract class JavaPlugin {
+	/**
+	 * 用户输入内容并回车时触发
+	 * @param cmd 用户输入的内容
+	 * @return 若为 false 则阻止这条消息被发送
+	 */
 	public boolean onCommand(String cmd) {
 		return true;
 	}
 	
-	public void onEnable() {
-	}
+	/**
+	 * 插件加载时触发
+	 */
+	public void onEnable() { }
 	
-	public void onDisable() {
-	}
+	/**
+	 * 插件加载完毕时触发
+	 */
+	public void onFinished() { }
 	
-	public void onGroupMessage(GroupMessageEvent event) {
-	}
+	/**
+	 * 插件禁用时触发，也会在关闭机器人时触发
+	 */
+	public void onDisable() { }
 	
-	public void onFinished() {
-	}
+	/**
+	 * 在已允许的群中有新消息时触发
+	 * @param event 新消息事件
+	 */
+	public void onGroupMessage(GroupMessageEvent event) { }
 }
