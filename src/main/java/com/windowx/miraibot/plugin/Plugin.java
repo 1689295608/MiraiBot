7e06b37474340ace131bae4585c5ef4dd89a8a1f
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2022-03-15 19:31:10 +0800

    移动 JavaPlugin 到 Plugin 内，改名 get/setClassLoader 为 get/setMyClassLoader

--- src/main/java/com/windowx/miraibot/plugin/Plugin.java
+++ src/main/java/com/windowx/miraibot/plugin/Plugin.java
@@ -28,11 +28,11 @@ public class Plugin extends JavaPlugin {
 		this.commands = commands;
 	}
 	
-	public ClassLoader getClassLoader() {
+	public ClassLoader getMyClassLoader() {
 		return classLoader;
 	}
 	
-	public void setClassLoader(ClassLoader classLoader) {
+	public void setMyClassLoader(ClassLoader classLoader) {
 		StackTraceElement[] stack = Thread.currentThread().getStackTrace();
 		for (StackTraceElement ste : stack) {
 			if (!ste.getClassName().equals("java.lang.Thread") && !ste.getClassName().equals("com.windowx.miraibot.PluginMain") &&
@@ -166,4 +166,36 @@ public class Plugin extends JavaPlugin {
 	public void warn(String warn) {
 		LogUtil.warn("[" + name + "] " + warn);
 	}
+}
+
+abstract class JavaPlugin {
+	/**
+	 * 用户输入内容并回车时触发
+	 * @param cmd 用户输入的内容
+	 * @return 若为 false 则阻止这条消息被发送
+	 */
+	public boolean onCommand(String cmd) {
+		return true;
+	}
+
+	/**
+	 * 插件加载时触发
+	 */
+	public void onEnable() { }
+
+	/**
+	 * 插件加载完毕时触发
+	 */
+	public void onFinished() { }
+
+	/**
+	 * 插件禁用时触发，也会在关闭机器人时触发
+	 */
+	public void onDisable() { }
+
+	/**
+	 * 在已允许的群中有新消息时触发
+	 * @param event 新消息事件
+	 */
+	public void onGroupMessage(GroupMessageEvent event) { }
 }
