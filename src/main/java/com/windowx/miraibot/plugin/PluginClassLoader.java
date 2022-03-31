1e02953d94d0bf75a98e9885ef86488a1ce692c7
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2022-03-31 19:45:46 +0800

    Update PluginClassLoader.java

--- src/main/java/com/windowx/miraibot/plugin/PluginClassLoader.java
+++ src/main/java/com/windowx/miraibot/plugin/PluginClassLoader.java
@@ -2,13 +2,38 @@ package com.windowx.miraibot.plugin;
 
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.HashMap;
 
 public class PluginClassLoader extends URLClassLoader {
-    public PluginClassLoader(URL[] urls, ClassLoader parent) {
+    private final HashMap<String, Class<?>> classes = new HashMap<>();
+    private final PluginLoader loader;
+
+    public PluginClassLoader(URL[] urls, ClassLoader parent, PluginLoader loader) {
         super(urls, parent);
+        this.loader = loader;
     }
 
     public Class<?> findClass(String name) throws ClassNotFoundException {
-        return super.findClass(name);
+        return findClass(name, true);
+    }
+
+    public Class<?> findClass(String name, boolean global) throws ClassNotFoundException {
+        Class<?> result = classes.get(name);
+
+        if (result == null) {
+            if (global) {
+                result = loader.getClassByName(name);
+            }
+
+            if (result == null) {
+                result = super.findClass(name);
+
+                if (result != null) {
+                    loader.setClass(name, result);
+                }
+            }
+            classes.put(name, result);
+        }
+        return result;
     }
 }
