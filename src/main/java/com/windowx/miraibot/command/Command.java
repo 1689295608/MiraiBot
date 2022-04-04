395fce74c2102c4fe8325e23742580ad3cd19dbb
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2022-04-04 17:44:21 +0800

    Create Command.java

---
+++ src/main/java/com/windowx/miraibot/command/Command.java
@@ -0,0 +1,27 @@
+package com.windowx.miraibot.command;
+
+public class Command {
+    private String name;
+    private String description;
+
+    public Command(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
