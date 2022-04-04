f64c93a7a0b341c4b47bd45720eb52937e9c8478
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2022-04-04 10:40:04 +0800

    新增 Logger 类

---
+++ src/main/java/com/windowx/miraibot/utils/Logger.java
@@ -0,0 +1,37 @@
+package com.windowx.miraibot.utils;
+
+public class Logger {
+    private String prefix = "";
+    public Logger() {
+        LogUtil.init();
+    }
+    public Logger(String prefix) {
+        this.prefix = prefix;
+        LogUtil.init();
+    }
+
+    public void info(String str) {
+        LogUtil.log_p(prefix, str);
+    }
+    public void info(String str, Object... args) {
+        LogUtil.log_p(prefix, str, args);
+    }
+
+    public void warn(String str) {
+        LogUtil.warn_p(prefix, str);
+    }
+    public void warn(String str, Object... args) {
+        LogUtil.warn_p(prefix, str, args);
+    }
+
+    public void error(String str) {
+        LogUtil.error_p(prefix, str);
+    }
+    public void error(String str, Object... args) {
+        LogUtil.error_p(prefix, str, args);
+    }
+    
+    public void clear() {
+        LogUtil.clear();
+    }
+}
