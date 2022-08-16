7fa43c451744396ed6f2357a84ce44afd7b8fa97
WindowX <1689295608@qq.com>
2022-08-16 16:26:00 +0800

    增加 logs 目录不存在的自动创建

--- src/main/java/com/windowx/miraibot/utils/LogUtil.java
+++ src/main/java/com/windowx/miraibot/utils/LogUtil.java
@@ -9,6 +9,8 @@ import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static com.windowx.miraibot.MiraiBot.logger;
+import static com.windowx.miraibot.utils.LanguageUtil.l;
 import static java.nio.file.StandardOpenOption.APPEND;
 import static java.nio.file.StandardOpenOption.CREATE;
 
@@ -21,6 +23,12 @@ public class LogUtil {
         Date date = new Date(System.currentTimeMillis());
         String time = formatter.format(date);
         File logs = new File("logs");
+        if (!logs.exists()) {
+            if (!logs.mkdir()) {
+                logger.error(l("cannot.mkdir"), logs.getName());
+                System.exit(-1);
+            }
+        }
         int i = 1;
         while (new File(logs, time + "-" + i + ".log").exists()) {
             i++;
